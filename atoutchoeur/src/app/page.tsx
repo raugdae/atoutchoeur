4e99013b97911a30f7ca8a24a3bdf7b1b6901c65
raugdae <raugdae@gmail.com>
2025-01-19 00:53:33 +0100

    Base welcome page

--- atoutchoeur/src/app/page.tsx
+++ atoutchoeur/src/app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+import verticalFlexbox from './components/css/VerticalFlexbox.module.css'
+import topBannerFlexbox from './components/css/TopBannerFlexbox.module.css'
+import middleBannerFlexbox from './components/css/MiddleBannerFlexbox.module.css'
+import cardGrid from './components/css/CardGrid.module.css'
+import menuContainer from './components/css/MenuContainer.module.css'
+import gridWrapper from './components/css/GridWrapper.module.css'
+import itemList from './components/css/ItemList.module.css'
+import navigationMenu from './components/css/NavigationMenu.module.css'
+import footerFlexbox from './components/css/FooterFlexbox.module.css'
+
+import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className={verticalFlexbox.verticalFlexbox}>
+      <div className={topBannerFlexbox.topBanner}>
+        <div className={topBannerFlexbox.topBannerImage}>
+          <Image src='/assets/top-banner.png' alt='Banniere' fill className='object-cover'/></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className={gridWrapper.gridWrapper}>
+        <div className={menuContainer.menuContainer}>
+          <div className={navigationMenu.navigationMenu}>Accueil</div>
+          <div className={navigationMenu.navigationMenu}>Présentation</div>
+          <div className={navigationMenu.navigationMenu}>Nous rejoindre</div>
+          <div className={navigationMenu.navigationMenu}>Nous contacter</div>
+        </div>
+      </div>
+      <div className={gridWrapper.gridWrapper}>
+        <div className={cardGrid.cardGrid}>
+          <div className={itemList.itemCard}>
+            <div className={itemList.itemTitle}><h1>Répétitions</h1></div>
+            <div className={itemList.itemContent}>
+            <p>Lieu : Collège de la Combe</p>
+            <p>Localité : Cugy</p>
+            <p>Horaire : Mardi de 20h à 22h</p> </div>
+          </div>
+          <div className={itemList.itemCard}>
+            <div className={itemList.itemTitle}>Prochain évènement</div>
+            <div className={itemList.itemContent}>
+              <p>Ensemble</p>
+            <p>55ème anniversaire de la Fanfare de la Police Cantonale Vaudoise</p>
+            </div>
+          </div>
+          <div className={itemList.itemCard}>
+            <div className={itemList.itemTitle}>Sur les réseaux sociaux</div>
+            <div className={itemList.itemContent}>
+              <p>Facebook</p>
+              <p>Instagram</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={middleBannerFlexbox.middleBanner}>
+        <div className={middleBannerFlexbox.middleBannerImage}>
+          <Image src='/assets/flyer-ensemble.png' alt='Flyer Ensemble' fill className='object-cover' />
+        </div>
+      </div>
+      <div className={footerFlexbox.footerFlexbox}>
+        <div className={footerFlexbox.footerFlexboxItem}>
+          Dévelopé par &nbsp;<a href='https://www.raug-info.ch'>raug-info.ch</a>&nbsp;- 2024 - Tout droit réservé
+        </div>
+
+      </div>
+      
     </div>
   );
 }
